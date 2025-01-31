e2f09faf0ae8e103f360dd37cf61374e70f8cd5c
imjsm <166825670+imjsm@users.noreply.github.com>
2025-01-31 20:39:06 +0530

    Update App.js

--- BinWise Frontend/src/App.js
+++ BinWise Frontend/src/App.js
@@ -11,7 +11,10 @@ import earth from "./assets/Screenshot 2025-01-25 234055.png";
 import BuyScrap from "./components/BuyScrap";
 import Login from "./components/login";
 import RegisteredAsScrapDealer from "./components/Register_as_scrap_dealer"
-
+import DragAndDropGame from "./components/DragAndDropGame";
+import Game from "./components/Game";
+import Login from "./components/login";
+import Quiz from "./components/Quiz";
 
 function App() {
  
@@ -88,11 +91,12 @@ function App() {
 
           {/* Services Route */}
           <Route path="/services" element={<ServicesPage />} />
-
+          <Route path="/DragAndDropGame" element={<DragAndDropGame />} />
+          <Route path="/quiz" element={<Quiz />} />
           {/* Placeholder for other pages */}
           <Route path="/scrapRates" element={<ScrapRates />} />
           <Route path="/login" element = {<Login/>}/>
-          <Route path="/contact" element={<h2>Contact us</h2>} />
+          <Route path="/game" element={ < Game />} />
           <Route path="/signup" element= {<RegisteredAsScrapDealer/>}/> 
           <Route path="/BuyScrap" element={<BuyScrap />} />
         </Routes>
